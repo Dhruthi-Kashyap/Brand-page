174df7e1a506816d8f7f62c0d1bac45629104944
Dhruthi-Kashyap <kdhruthim@gmail.com>
2024-12-05 10:54:25 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import Navigation from "./components/navigation";
+const App = () => {
+return <div>
+        <Navigation />
+        <HeroSection />
+    </div>
+};
+export default App;
